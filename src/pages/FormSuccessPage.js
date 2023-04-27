9ffb33cfa935367963804a20d112df9d9a891759
zainuleb <zainulebadd@gmail.com>
2023-04-27 07:11:35 +0500

    success-rev2-27-04

--- src/pages/FormSuccessPage.js
+++ src/pages/FormSuccessPage.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const FormSuccessPage = () => {
-  return <div>FormSuccessPage</div>;
+  return (
+    <>
+      <div className="breadcrumb__section include-bg overlay_success ">
+        <div className="container">
+          <div className="row">
+            <div className="col-xxl-12">
+              <div className="breadcrumb__content d-md-flex justify-content-between align-items-center">
+                <h3 className="breadcrumb__title">
+                  Thank you!
+                  <br />
+                  Your form has been submitted
+                </h3>
+                <div className="breadcrumb__list"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FormSuccessPage;
