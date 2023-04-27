3903ba5f7f7396afab7ecf8416f45dc188ee580e
zainuleb <zainulebadd@gmail.com>
2023-04-27 05:40:34 +0500

    membership-rev2-27-04

--- src/component/aboutUs/AboutUs.js
+++ src/component/aboutUs/AboutUs.js
@@ -13,31 +13,48 @@ const AboutUs = () => {
           <div className="col-lg-6">
             <div className="bd-about__text mb-30 wow fadeInLeft">
               <div className="bd-section__title-wrapper">
-                {/* <span className="bd-section__subtitle bd-section__subtitle-lg mb-10">
-                  Brians behind DriveSphere
-                </span> */}
-                <h3 className="bd-section__title bd-section__title-lg mb-25">
-                  Brians behind DriveSphere
-                </h3>
+                <div className="about_us_header d-flex align-items-center mb-20">
+                  <img src="assets/img/logo/logo.png" alt="logo" />
+                  <h3 className="bd-section__title bd-section__title-lg">
+                    DriveSphere
+                  </h3>
+                </div>
                 <p>
                   DriveSphere is the leading driving simulator training center
                   in Europe managed by Lorena Andrade and Zain Ul Ebad. Our
                   high-quality simulator-based training is tailored to meet the
                   standards of all European Union driving associations and legal
-                  road traffic regulations. Our training program is backed by
-                  data and research, and is continuously updated to ensure that
-                  they are up-to-date with the latest road challenges.
+                  road traffic regulations.
                 </p>
-                <p className="mb-25">
-                  Our training program has proven benefits for drivers,
-                  including improved reaction times, enhanced driving skills,
-                  and increased confidence on the road. We achieve this by using
-                  realistic and interactive scenarios that help drivers learn
-                  how to handle real-life driving situations safely and
-                  effectively. Our training program is backed by data and
-                  research, and is continuously updated to ensure that they are
-                  up-to-date with the latest road challenges.
-                </p>
+                <div className="bd-section__inner-list mb-40">
+                  <ul>
+                    <li>
+                      <span>
+                        <i class="fas fa-angle-double-right"></i>
+                      </span>
+                      Our training program is
+                      <strong> backed by hundreds of data research,</strong> and
+                      is continuously updated to ensure that they are up-to-date
+                      with the latest road challenges.
+                    </li>
+                    <li>
+                      <span>
+                        <i class="fas fa-angle-double-right"></i>
+                      </span>
+                      It has proven benefits for drivers, including improved
+                      reaction times, enhanced driving skills, and increased
+                      confidence on the road. Leading to up to{" "}
+                      <strong>60% of reduced road accidents.</strong>
+                    </li>
+                    <li>
+                      <span>
+                        <i class="fas fa-angle-double-right"></i>
+                      </span>
+                      With over 2.000 monthly active members, we are the biggest
+                      simulator training center in Europe.
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -6,63 +6,57 @@ const Main = () => {
       <div className="logo_section">
         <img src="assets/img/logo/logo.png" alt="logo.png" />
       </div>
-      <div className="bd-slider__section">
-        <div className="bd-slider__active bd-slider__wrapper swiper-container">
-          <div className="swiper-wrapper">
-            <div
-              className="swiper-slide bd-slider__item bd-slider__height include-bg d-flex align-items-center bg-item"
-              // style={{
-              //   backgroundImage: "url(assets/img/extra/manDriving.png)",
-              //   backgroundSize: "65em 75em",
-              //   backgroundPosition: "36rem -9rem",
-              //   backgroundRepeat:
-              //     "no-repeat" /* prevent the background from repeating */,
-              //   width: "100%" /* set the width to fill the container */,
-              //   height: "100%" /* set the height to fill the container */,
-              //   position: "relative" /* make the item position relative */,
-              // }}
-            >
-              <div className="container">
-                <div className="row">
-                  <div className="col-xxl-6 col-xl-8 col-lg-8 col-md-10">
-                    <div className="bd-slider__content">
-                      <span
-                        className="bd-slider__subtitle"
-                        data-animation="fadeInUp"
-                        data-delay=".3s"
-                      >
-                        Confident Drivers
-                      </span>
-                      <h5
-                        className="bd-slider__title"
-                        data-animation="fadeInUp"
-                        data-delay=".5s"
-                      >
-                        Trained by Innovative, Data Driven Technologies
-                      </h5>
-                      <p
-                        className="mainPara"
-                        data-animation="fadeInUp"
-                        data-delay=".7s"
-                      >
-                        Enroll in our EU training program to prepare for your EU
-                        Digital Driver's License and achieve a safer and faster
-                        driving experience.
-                      </p>
-                      <div
-                        className="bd-slider__btn"
-                        data-animation="fadeInUp"
-                        data-delay=".9s"
-                      >
-                        <a href="about.html" className="bd-btn">
-                          Get Free Simulator Class NOW
-                          <span>
-                            <i className="fa-regular fa-angle-right"></i>
-                          </span>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+      <div
+        className="swiper-slide bd-slider__item bd-slider__height include-bg d-flex align-items-center bg-item"
+        // style={{
+        //   backgroundImage: "url(assets/img/extra/manDriving.png)",
+        //   backgroundSize: "65em 75em",
+        //   backgroundPosition: "36rem -9rem",
+        //   backgroundRepeat:
+        //     "no-repeat" /* prevent the background from repeating */,
+        //   width: "100%" /* set the width to fill the container */,
+        //   height: "100%" /* set the height to fill the container */,
+        //   position: "relative" /* make the item position relative */,
+        // }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-xxl-6 col-xl-8 col-lg-8 col-md-10">
+              <div className="bd-slider__content">
+                <span
+                  className="bd-slider__subtitle"
+                  data-animation="fadeInUp"
+                  data-delay=".3s"
+                >
+                  Confident Drivers
+                </span>
+                <h5
+                  className="bd-slider__title"
+                  data-animation="fadeInUp"
+                  data-delay=".5s"
+                >
+                  Trained by Innovative, Data Driven Technologies
+                </h5>
+                <p
+                  className="mainPara"
+                  data-animation="fadeInUp"
+                  data-delay=".7s"
+                >
+                  Enroll in our EU training program to prepare for your EU
+                  Digital Driver's License and achieve a safer and faster
+                  driving experience.
+                </p>
+                <div
+                  className="bd-slider__btn"
+                  data-animation="fadeInUp"
+                  data-delay=".9s"
+                >
+                  <a href="about.html" className="bd-btn">
+                    Get Free Simulator Class NOW
+                    <span>
+                      <i className="fa-regular fa-angle-right"></i>
+                    </span>
+                  </a>
                 </div>
               </div>
             </div>
